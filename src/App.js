84ca33d681410c1fd874ff9df90c6d17fef11dee
Raj-Satyam1206 <raj.satyam1229@gmail.com>
2024-11-15 02:17:39 +0530

    SignIn/SignUp && Login page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-import './App.css';
+import Body from "./components/Body";
 
 function App() {
-  return (
-    <div className="App">
-      Namaste Everyone, we are building Netflix GPT
-    </div>
-  );
+  return <Body />;
 }
 
 export default App;
