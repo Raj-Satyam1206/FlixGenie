de61f247cfea204367d183d46a393dddc69a3788
Raj-Satyam1206 <raj.satyam1229@gmail.com>
2024-11-26 18:58:33 +0530

    Implemented Multi-lingual feature completely for GPTSearch & Browse page && Implemented Homepage button

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,6 +6,7 @@ import { addUser, removeUser } from "../utils/userSlice";
 import { useEffect} from "react";
 import { LOGO , SUPPORTED_LANGUAGES, USER_AVATAR} from "../utils/constants";
 import { toggleGptSearchView } from "../utils/gptSlice";
+import { changeLanguage } from "../utils/configSlice";
 
 
 const Header = () => {
@@ -13,6 +14,7 @@ const Header = () => {
   const navigate = useNavigate(); 
   const dispatch = useDispatch();
   const user = useSelector((store) => store.user);
+  const showGptSearch = useSelector((store) => store.gpt.showGptSearch);
 
   const handleSignOut = () =>{
     signOut(auth).then(() => {
@@ -26,6 +28,11 @@ const Header = () => {
     //Toggle GPT Search
     dispatch(toggleGptSearchView());
   }
+
+  const handleLanguageChange = (e) => {
+    // console.log(e.target.value);
+    dispatch(changeLanguage(e.target.value));
+  };
   
   useEffect(()=>{
     const unsubscribe = onAuthStateChanged(auth, (user) => {
@@ -55,27 +62,30 @@ const Header = () => {
       />
       {user && (
         <div className="flex p-2">
-          <select 
-            className="p-2 m-2 bg-gray-900 text-white"
-            onChange={handleLanguageChange}
-          >
+          {showGptSearch && (
+            <select 
+              className="p-2 m-2 bg-gray-900 text-white"
+              onChange={handleLanguageChange}
+            >
             {/* <option value="en">English</option>
             <option value="spanish">Spanish</option>
             <option value="hindi">Hindi</option> */}
-            {SUPPORTED_LANGUAGES.map((lang) => (
-              <option 
-                key={lang.identifier} 
-                value={lang.identifier}
-              >  
-                {lang.name}
-              </option>
-            ))}
-          </select>
+              {SUPPORTED_LANGUAGES.map((lang) => (
+                <option 
+                  key={lang.identifier} 
+                  value={lang.identifier}
+                >  
+                  {lang.name}
+                </option>
+              ))}
+            </select>
+            )
+          }
           <button
             className="py-2 px-4 mx-4 my-2 bg-purple-800 text-white rounded-lg"
             onClick={handleGptSearchClick}
           >
-            GPT Search
+            {showGptSearch ? "HomePage" : "GPT Search"}
           </button>
           <img 
             className="w-12 h-12"
