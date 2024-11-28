d56b3433b0c504cb3396ae965d6cddb9bb54b441
Raj-Satyam1206 <raj.satyam1229@gmail.com>
2024-11-28 17:32:07 +0530

    Fix: Ensure background covers full screen across all device sizes

--- src/components/Login.js
+++ src/components/Login.js
@@ -84,55 +84,55 @@ const Login = () => {
       <Header />
       <div className='absolute'>
           <img 
-            className="h-screen object-cover"
+            className="w-screen h-screen object-cover"
             src={BG_URL}
             alt="Background Page" 
           />
       </div>
 
       <form 
-          onSubmit={(e) => e.preventDefault()} 
-          className='w-full md:w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80'
-          >
+        onSubmit={(e) => e.preventDefault()} 
+        className='w-full md:w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80'
+      >
 
-          <h1 className='font-bold text-3xl py-4'>
-              {isSignInForm ? "Sign In" : "Sign Up" }
-          </h1>
+        <h1 className='font-bold text-3xl py-4'>
+            {isSignInForm ? "Sign In" : "Sign Up" }
+        </h1>
 
-          {
-            !isSignInForm && 
-                (<input ref={name} type="text" placeholder='Full Name' className='p-4 my-4 w-full bg-gray-700' />) 
-          }
-          <input
-              ref={email} 
-              type="text" 
-              placeholder='Email Address' 
-              className='p-4 my-4 w-full bg-gray-700' 
-          />
-          <input 
-              ref={password}
-              type="password" 
-              placeholder='Password' 
-              className='p-4 my-4 w-full bg-gray-700' 
-          />
+        {
+          !isSignInForm && 
+              (<input ref={name} type="text" placeholder='Full Name' className='p-4 my-4 w-full bg-gray-700' />) 
+        }
+        <input
+            ref={email} 
+            type="text" 
+            placeholder='Email Address' 
+            className='p-4 my-4 w-full bg-gray-700' 
+        />
+        <input 
+            ref={password}
+            type="password" 
+            placeholder='Password' 
+            className='p-4 my-4 w-full bg-gray-700' 
+        />
 
-          <p className='text-red-500 font-bold text-lg py-2'>{errormessage}</p>
+        <p className='text-red-500 font-bold text-lg py-2'>{errormessage}</p>
 
-          <button 
-              className='p-4 my-6 bg-red-700 w-full rounded-lg' 
-              onClick = {handleValidate}
-            >   
-              {isSignInForm ? "Sign In" : "Sign Up" }
-          </button>
-          <p 
-              className='py-4 cursor-pointer' 
-              onClick={handleSignIn}>
+        <button 
+            className='p-4 my-6 bg-red-700 w-full rounded-lg' 
+            onClick = {handleValidate}
+          >   
+            {isSignInForm ? "Sign In" : "Sign Up" }
+        </button>
+        <p 
+            className='py-4 cursor-pointer' 
+            onClick={handleSignIn}>
 
-                {isSignInForm ? "New to Netflix? Sign Up Now !!" : "Already a User? Sign In Now !!"}
-          </p>
+              {isSignInForm ? "New to Netflix? Sign Up Now !!" : "Already a User? Sign In Now !!"}
+        </p>
       </form>
     </div>
-  )
-}
+  );
+};
 
 export default Login;
