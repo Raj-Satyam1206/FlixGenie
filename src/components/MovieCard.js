9bcd18f5c2e939d24548533798bac6e9f77b527d
Raj-Satyam1206 <raj.satyam1229@gmail.com>
2024-11-29 03:33:38 +0530

    Updated the design and layout of the Movie Card component

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -4,11 +4,14 @@ const MovieCard = ({posterPath}) => {
 
     if(!posterPath) return null;
   return (
-    <div className="w-36 md:w-48 pr-4">
-        <img 
-            alt="Movie Card" 
-            src={IMG_CDN_URL + posterPath}    
+    <div className="w-36 md:w-48 pr-4 transform transition-all duration-300 group">
+        <div className="relative group">
+        <img
+          alt="Movie Card"
+          src={IMG_CDN_URL + posterPath}
+          className="w-full h-full object-cover rounded-md transition-transform duration-300 transform group-hover:scale-110 group-hover:shadow-2xl"
         />
+    </div>
     </div>
   );
 };
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,11 +1,11 @@
 import MovieCard from "./MovieCard";
 
 const MovieList = ({title , movies}) => {
-    console.log(movies);
+    // console.log(movies);
   return ( 
     <div className="px-6">
-        <h1 className="text-lg md:text-3xl py-4 text-white">{title}</h1>
-        <div className="flex overflow-x-scroll scrollbar-hide">
+        <h1 className="text-lg md:text-2xl font-serif py-4 text-white">{title}</h1>
+        <div className="flex overflow-x-scroll scrollbar-hide space-x-4 p-4 bg-gray-950">
             <div className="flex">
                 {movies?.map((movie) => (
                     <MovieCard key={movie.id} posterPath={movie.poster_path} />
