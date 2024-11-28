88698b6bc660fa6bb927f1c33ec29aba6350b317
Raj-Satyam1206 <raj.satyam1229@gmail.com>
2024-11-28 17:00:37 +0530

    Added Responsiveness for seamless experience on smaller devices and tablets

--- src/components/GptSearch.js
+++ src/components/GptSearch.js
@@ -5,17 +5,20 @@ import { BG_URL } from '../utils/constants';
 
 const GptSearch = () => {
   return (
-    <div>
-        <div className='fixed -z-10'>
-            <img 
-              src= {BG_URL}
-              alt="Background Page" 
-            />
-        </div>
+    <>
+      <div className='fixed -z-10'>
+          <img 
+            className="h-screen object-cover"
+            src= {BG_URL}
+            alt="Background Page" 
+          />
+      </div>
+      <div className=''>
         <GptSearchBar />
         <GptMovieSuggestions />
-    </div>
-  )
-}
+      </div>
+    </>
+  );
+};
 
 export default GptSearch;
