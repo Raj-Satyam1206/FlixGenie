c5c2eba284a5ce4cd31b03b842ff601041b6627c
Raj-Satyam1206 <raj.satyam1229@gmail.com>
2025-01-06 17:30:23 +0530

    Add Link tag to each card for navigating to movie details and trailer

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,18 +1,31 @@
+import { Link } from "react-router-dom";
 import { IMG_CDN_URL } from "../utils/constants";
 
-const MovieCard = ({posterPath}) => {
+const MovieCard = ({movie}) => {
+  // const navigate = useNavigate();
 
-    if(!posterPath) return null;
+    if(!movie?.poster_path) return null;
+
+    // const handleClick = () => {
+    //   console.log("Movie Details:", movie);
+    //   navigate(`/movie/${movie.id}`); 
+    // };
+
   return (
-    <div className="w-36 md:w-48 pr-4 transform transition-all duration-300 group">
+    <Link to={`/movie/${movie.id}`}> 
+      <div className="w-36 md:w-48 pr-4 transform transition-all duration-300 group"
+        // onClick={handleClick}
+      >
         <div className="relative group">
-        <img
+          <img
           alt="Movie Card"
-          src={IMG_CDN_URL + posterPath}
+          src={IMG_CDN_URL + movie.poster_path}
           className="w-full h-full object-cover rounded-md transition-transform duration-300 transform group-hover:scale-110 group-hover:shadow-2xl"
-        />
-    </div>
-    </div>
+          />
+        </div>
+      </div>
+    </Link>
+    
   );
 };
 
