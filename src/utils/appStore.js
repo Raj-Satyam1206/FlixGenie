4bcb9becf67df117d56371f3efd3657ed48cb9c7
Raj-Satyam1206 <raj.satyam1229@gmail.com>
2025-01-11 14:00:47 +0530

    Integrated Redux Persist for state persistence in userReducer using persistReducer and persistStore

--- src/utils/appStore.js
+++ src/utils/appStore.js
@@ -3,14 +3,27 @@ import userReducer from "./userSlice";
 import moviesReducer from "./moviesSlice";
 import gptReducer from "./gptSlice";
 import configReducer from "./configSlice";
+import persistReducer from "redux-persist/es/persistReducer";
+import persistStore from "redux-persist/es/persistStore";
+import storage from "redux-persist/lib/storage"
+
+
+const persistConfig = {
+    key: "root",
+    storage,
+    whitelist: ["user"], // Persist only the user slice
+  };
+  
+  const persistedUserReducer = persistReducer(persistConfig, userReducer);
 
 const appStore = configureStore({
     reducer: {
-        user : userReducer,
+        user : persistedUserReducer,
         movies : moviesReducer,
         gpt : gptReducer,
         config : configReducer,
     },
 });
 
+export const persistor = persistStore(appStore);
 export default appStore;
